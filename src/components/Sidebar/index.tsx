d5bb5eaea03d1223b4623daa598da5b25db949ad
Gustavo Kimura <gustavokimuraf@gmail.com>
2025-11-04 09:31:35 -0300

    refacytor: remove chevron buttons on Sidebar menu

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,5 @@
-import { ChevronLeft, ChevronRight, Menu } from "lucide-react";
+import { Menu } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import { SidebarNav } from "../SidebarNav";
 import { useRef } from "react";
 
@@ -37,15 +36,12 @@ export default function Sidebar({
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      <div className="flex h-14 items-center justify-between p-4 md:h-16 cursor-pointer">
+      <div className="flex h-14 items-center p-4 md:h-16">
         <div
           className={cn(
-            "flex items-center gap-2",
+            "flex items-center gap-2 w-full",
             isDesktopClosed && "justify-center"
           )}
-          role="button"
-          tabIndex={0}
-          aria-label="Alternar navegação"
         >
           <Menu className="h-6 w-6" />
           <span
@@ -54,21 +50,6 @@ export default function Sidebar({
             Administração
           </span>
         </div>
-
-        <Button
-          variant="ghost"
-          size="icon"
-          className={cn(
-            "text-primary-foreground hover:bg-primary/80 hover:text-primary-foreground",
-            isDesktopClosed && "mr-1"
-          )}
-        >
-          {isDesktopClosed ? (
-            <ChevronRight className="h-4 w-4" />
-          ) : (
-            <ChevronLeft className="h-4 w-4" />
-          )}
-        </Button>
       </div>
 
       <div className="flex-1 overflow-auto py-2">
