c2a8cd55e36accafbd3403a49c06ddafc42a0cd6
Gustavo Kimura <gustavokimuraf@gmail.com>
2025-12-03 19:49:00 -0300

    feat(web): align theme architecture with mobile project

--- src/shared/components/BaseCard.tsx
+++ src/shared/components/BaseCard.tsx
@@ -1,5 +1,3 @@
 import styled from "@emotion/styled";
 
-export const BaseCard = styled.div`
-  ${props => props.theme.cardVariants.elevated}
-`;
+export const BaseCard = styled.div(props => props.theme.cardVariants.elevated);
