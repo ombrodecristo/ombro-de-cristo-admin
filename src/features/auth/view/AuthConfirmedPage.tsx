6f1faa60066b8c7e67e3db5cf6de528a8bef7c45
Gustavo Kimura <gustavokimuraf@gmail.com>
2025-12-04 15:12:21 -0300

    fix(ui): center content on auth feedback pages

--- src/features/auth/view/AuthConfirmedPage.tsx
+++ src/features/auth/view/AuthConfirmedPage.tsx
@@ -8,7 +8,7 @@ const PageContainer = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
   padding: ${props => props.theme.spacing.m}px;
 `;
 
